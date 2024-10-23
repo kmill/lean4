bcfced4fd964bfffddd22fa5e57868b3bb5ffa5d
Kyle Miller <kmill31415@gmail.com>
2024-10-22 22:30:14 -0700

    more unique

--- src/Lean/Util/Sorry.lean
+++ src/Lean/Util/Sorry.lean
@@ -11,19 +11,19 @@ namespace Lean
 
 def Expr.isSorry : Expr → Bool
   | mkApp2 (.const ``sorryAx ..) _ _ => true
-  -- A unique sorry:
+  -- A labeled sorry:
   | mkApp3 (.const ``sorryAx ..) _ _ _ => true
   | _ => false
 
 def Expr.isSyntheticSorry : Expr → Bool
   | mkApp2 (const ``sorryAx ..) _ (const ``Bool.true ..) => true
-  -- A unique sorry:
+  -- A labeled sorry:
   | mkApp3 (const ``sorryAx ..) _ (const ``Bool.true ..) _ => true
   | _ => false
 
 def Expr.isNonSyntheticSorry : Expr → Bool
   | mkApp2 (const ``sorryAx ..) _ (const ``Bool.false ..) => true
-  -- A unique sorry:
+  -- A labeled sorry:
   | mkApp3 (const ``sorryAx ..) _ (const ``Bool.false ..) _ => true
   | _ => false
 
